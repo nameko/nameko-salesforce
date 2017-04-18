681fb4d94767d3beae0c7832fbeb5c84de7016b2
Ondrej Kohout <ondrej.kohout@gmail.com>
2017-04-18 21:02:38 +0100

    minor docstring changes

--- nameko_salesforce/streaming/client.py
+++ nameko_salesforce/streaming/client.py
@@ -65,23 +65,21 @@ class SalesForceBayeuxClient(BayeuxClient):
         self.replay_enabled = False
         """ PushTopic Events tracking enabled
 
-        If set to ``True``, the client will persist last event Reply ID for each
-        channel and use it when subscribing to immediately receive events missed
-        during the retention window.
+        If set to ``True``, the client will persist last event Reply ID for
+        each channel and use it when subscribing to immediately receive events
+        missed during the retention window.
 
         """
 
         self.replay_storage = None
-        """
-        Redis client instance for persisting PushTopic Replay IDs
+        """ Redis client instance for persisting PushTopic Replay IDs
 
         Initialized only if ``replay_enabled`` is set.
 
         """
 
         self.replay_storage_ttl = constants.DEFAULT_REPLAY_STORAGE_TTL
-        """
-        Time to live for persisted PushTopic Reply IDs
+        """ Time to live for persisted PushTopic Reply IDs
 
         Salesforce promises to keep events for 24 hours, therefore the TTL
         value must not exceed it.
@@ -177,7 +175,6 @@ class SalesForceBayeuxClient(BayeuxClient):
         pipe.set(key, replay_id)
         pipe.expire(key, self.replay_storage_ttl)
         pipe.execute()
-
 
 
 class SalesforceMessageHandler(BayeuxMessageHandler):
