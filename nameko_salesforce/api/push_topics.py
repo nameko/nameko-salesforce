1e348b9c77efde412c30ba627ba9b195ffbb6b5e
Ondrej Kohout <ondrej.kohout@gmail.com>
2017-05-07 12:25:36 +0100

    Add push topic declaration

--- nameko_salesforce/api/push_topics.py
+++ nameko_salesforce/api/push_topics.py
@@ -31,6 +31,61 @@ class PushTopicsAPIClient(ClientProxy):
         self.config = config
         super().__init__(pool)
 
+    def declare_push_topic(
+        self, sobject_type, record_type=None, exclude_current_user=False
+    ):
+        """
+        Update or create Push Topic object
+
+        Note that the update or create operation is not atomic and there
+        is a race condition with multiple services trying to declare the topic
+        at the same time.
+
+        """
+
+        if record_type:
+            name = '{}{}'.format(sobject_type, record_type)
+            record_type_id = self.get_record_type_id_by_name(
+                sobject_type, record_type)
+        else:
+            name = sobject_type
+
+        push_topic_query = (
+            "SELECT Id, Name, LastModifiedById, LastModifiedDate FROM {}"
+            .format(sobject_type))
+
+        conditions = []
+        if record_type:
+            conditions.append(
+                "RecordTypeId = '{}'".format(record_type_id))
+        if exclude_current_user:
+            current_user_id = self.get_user_id_by_name(
+                self.config['SALESFORCE']['USERNAME'])
+            conditions.append(
+                "LastModifiedById != '{}'".format(current_user_id))
+
+        if conditions:
+            push_topic_query = '{} WHERE {}'.format(
+                push_topic_query, ' AND '.join(conditions))
+
+        push_topic_data = {
+            'Name': name,
+            'Query': push_topic_query,
+            'ApiVersion': self.config['SALESFORCE']['API_VERSION'],
+            'NotifyForOperationCreate': True,
+            'NotifyForOperationUpdate': True,
+            'NotifyForOperationUndelete': True,
+            'NotifyForOperationDelete': True,
+            'NotifyForFields': 'All',
+        }
+
+        try:
+            record = self.get_push_topic_by_name(name)
+        except NotFound:
+            self.PushTopic.create(push_topic_data)
+        else:
+            self.PushTopic.update(record['Id'], push_topic_data)
+
     @cached
     def get_push_topic_by_name(self, name):
         query = (
--- tests/api/test_push_topics.py
+++ tests/api/test_push_topics.py
@@ -4,7 +4,7 @@ import requests_mock
 from simple_salesforce import SalesforceResourceNotFound
 
 from nameko_salesforce.api.client import ClientProxy
-from nameko_salesforce.api.push_topics import get_client
+from nameko_salesforce.api.push_topics import get_client, PushTopicsAPIClient
 
 
 @pytest.fixture
@@ -191,3 +191,162 @@ def test_get_record_type_id_by_name_cached(
         sobject_type_name, record_typ_name)
     assert result == '00..A0'
     assert len(mock_salesforce_server.request_history) == 1  # requested once
+
+
+class TestDeclarePushTopic:
+
+    def test_push_topic_created(
+        self, api_version, client, mock_salesforce_server
+    ):
+        """ Test creates push topic if it does not exist
+        """
+        sobject_type = 'Contact'
+
+        # setup push topic lookup
+        response_data = {'totalSize': 0, 'records': []}
+        mock_salesforce_server.get(requests_mock.ANY, json=response_data)
+
+        # setup push topic creation
+        mock_salesforce_server.post(requests_mock.ANY, json={})
+
+        client.declare_push_topic(sobject_type)
+
+        get_request, post_request = mock_salesforce_server.request_history
+
+        assert get_request.method == 'GET'
+        assert get_request.path == '/services/data/v{}/query/'.format(api_version)
+
+        assert post_request.method == 'POST'
+        assert post_request.path == (
+            '/services/data/v{}/sobjects/pushtopic/'.format(api_version))
+
+    def test_push_topic_updated(
+        self, api_version, client, mock_salesforce_server
+    ):
+        """ Test updates push topic if it exists
+        """
+        sobject_type = 'Contact'
+
+        # push topic lookup response
+        response_data = {'totalSize': 1, 'records': [{'Id': '00..A0'}]}
+        mock_salesforce_server.get(requests_mock.ANY, json=response_data)
+
+        # push topic update response
+        mock_salesforce_server.patch(requests_mock.ANY, json={})
+
+        client.declare_push_topic(sobject_type)
+
+        get_request, post_request = mock_salesforce_server.request_history
+
+        assert get_request.method == 'GET'
+        assert get_request.path == '/services/data/v{}/query/'.format(api_version)
+
+        assert post_request.method == 'PATCH'
+        assert post_request.path == (
+            '/services/data/v{}/sobjects/pushtopic/00..a0'.format(api_version))
+
+    @pytest.fixture
+    def get_record_type_id_by_name(self):
+        with patch.object(
+            PushTopicsAPIClient, 'get_record_type_id_by_name'
+        ) as method:
+            yield method
+
+    @pytest.fixture
+    def get_user_id_by_name(self):
+        with patch.object(
+            PushTopicsAPIClient, 'get_user_id_by_name'
+        ) as method:
+            yield method
+
+    @pytest.fixture
+    def get_push_topic_by_name(self):
+        with patch.object(
+            PushTopicsAPIClient, 'get_push_topic_by_name'
+        ) as method:
+            yield method
+
+    @pytest.fixture(params=('create', 'update'))
+    def write_push_topic(
+        self, request, client, get_push_topic_by_name, mock_salesforce_server
+    ):
+        if request.param == 'create':
+            get_push_topic_by_name.side_effect = client.NotFound
+            create_push_topic = mock_salesforce_server.post(
+                requests_mock.ANY, json={})
+            return create_push_topic
+        if request.param == 'update':
+            get_push_topic_by_name.return_value = {'Id': '00..A0'}
+            update_push_topic = mock_salesforce_server.patch(
+                requests_mock.ANY, json={})
+            return update_push_topic
+
+    @pytest.mark.parametrize(
+        ('sobject_type', 'record_type', 'expected_name'),
+        (
+            ('Contact', None, 'Contact'),
+            ('Contact', 'Student', 'ContactStudent'),
+        )
+    )
+    def test_push_topic_name(
+        self, get_record_type_id_by_name, client, write_push_topic,
+        sobject_type, record_type, expected_name
+    ):
+        """ Test push topic name definition
+
+        Parameterised as a matrix testing the cases for creation of a new
+        push topic object and for an update of existing one.
+
+        """
+        client.declare_push_topic(sobject_type, record_type)
+
+        request = write_push_topic.request_history[0]
+        assert request.json()['Name'] == expected_name
+
+    @pytest.mark.parametrize(
+        ('record_type', 'exclude_current_user', 'expected_query'),
+        (
+            (
+                None, False,
+                ("SELECT Id, Name, LastModifiedById, LastModifiedDate "
+                 "FROM Contact"),
+            ),
+            (
+                'Student', False,
+                ("SELECT Id, Name, LastModifiedById, LastModifiedDate "
+                 "FROM Contact WHERE RecordTypeId = '00..A0'"),
+            ),
+            (
+                None, True,
+                ("SELECT Id, Name, LastModifiedById, LastModifiedDate "
+                 "FROM Contact WHERE LastModifiedById != '11..A1'"),
+            ),
+            (
+                'Student', True,
+                ("SELECT Id, Name, LastModifiedById, LastModifiedDate "
+                 "FROM Contact WHERE RecordTypeId = '00..A0' "
+                 "AND LastModifiedById != '11..A1'"),
+            ),
+        )
+    )
+    def test_push_topic_query(
+        self, get_record_type_id_by_name, get_user_id_by_name, client,
+        write_push_topic, record_type, exclude_current_user, expected_query
+    ):
+        """ Test push topic query definition
+
+        Parameterised as a matrix testing the cases for creation of a new
+        push topic object and for an update of existing one.
+
+        """
+        sobject_type = 'Contact'
+
+        get_record_type_id_by_name.return_value = '00..A0'
+        get_user_id_by_name.return_value = '11..A1'
+
+        client.declare_push_topic(
+            sobject_type, record_type,
+            exclude_current_user=exclude_current_user)
+
+        request = write_push_topic.request_history[0]
+        assert request.json()['Query'] == expected_query
